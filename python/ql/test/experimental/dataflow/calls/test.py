affb1ab88395fcdb01af5e57667469ced4223034
Rasmus Wriedt Larsen <rasmuswl@github.com>
2022-06-22 17:13:19 +0200

    Python: Add staticmethod/classmethod to `dataflow/calls`

--- python/ql/test/experimental/dataflow/calls/test.py
+++ python/ql/test/experimental/dataflow/calls/test.py
@@ -14,16 +14,33 @@ class MyClass(object):
     def my_method(self, arg):
         pass
 
+    @staticmethod # $ arg[position 0]=FunctionExpr call=staticmethod(..) qlclass=NormalCall
+    def staticmethod(arg):
+        pass
+
+    @classmethod # $ arg[position 0]=FunctionExpr call=classmethod(..) qlclass=NormalCall
+    def classmethod(cls, arg):
+        pass
+
     def __getitem__(self, key):
         pass
 
 
-func("foo") # $ call=func(..) qlclass=NormalCall arg[position 0]="foo"
+func(0) # $ call=func(..) qlclass=NormalCall arg[position 0]=0
+
 x = MyClass(1) # $ call=MyClass(..) qlclass=NormalCall arg[position 0]=1
+
 x.my_method(2) # $ call=x.my_method(..) qlclass=NormalCall arg[position 0]=2 MISSING: arg[self]=x
 mm = x.my_method
 mm(2) # $ call=mm(..) qlclass=NormalCall arg[position 0]=2 MISSING: arg[self]=x
-x[3] # $ MISSING: call=x[3] qlclass=SpecialCall arg[position 0]=x arg[position 1]=3
+
+x.staticmethod(3) # $ arg[position 0]=3 call=x.staticmethod(..) qlclass=NormalCall
+MyClass.staticmethod(3) # $ arg[position 0]=3 call=MyClass.staticmethod(..) qlclass=NormalCall
+
+x.classmethod(4) # $ arg[position 0]=4 call=x.classmethod(..) qlclass=NormalCall
+MyClass.classmethod(4) # $ arg[position 0]=4 call=MyClass.classmethod(..) qlclass=NormalCall
+
+x[5] # $ MISSING: call=x[5] qlclass=SpecialCall arg[self]=x arg[position 0]=5
 
 
 try:
